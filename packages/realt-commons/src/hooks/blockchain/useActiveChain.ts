dd15bd3ac3fa52a4fc8502ff12dca5c6e5f704aa
jeanbenoit-richez <jeanbenoit@realt.co>
2023-05-02 11:56:35 -0400

    fix: error in useActiveChain

--- packages/realt-commons/src/hooks/blockchain/useActiveChain.ts
+++ packages/realt-commons/src/hooks/blockchain/useActiveChain.ts
@@ -1,11 +1,9 @@
-import { useMemo } from 'react';
+import { Chain, ChainSelectConfig } from "@realtoken/realt-commons";
+import { useMemo } from "react";
 import { useWeb3React } from '@web3-react/core';
-import { ChainsID, CHAINS } from '../../config/constants/chain';
-import { Chain, ChainSelectConfig } from '../../types';
-
 
 export function useActiveChain<T extends Partial<Chain>>(chainConfig: ChainSelectConfig<T>): T|undefined {
-  const { chainId } = useWeb3React();
-
-  return useMemo(() => chainId && chainId in ChainsID ? chainConfig.chainsConfig[chainId as ChainsID] : undefined,[chainId]);
-};
+    const { chainId } = useWeb3React();
+  
+    return useMemo(() => chainId ? chainConfig.chainsConfig[chainId] : undefined,[chainConfig.chainsConfig, chainId]);
+  };
