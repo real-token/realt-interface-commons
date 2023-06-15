6ee9775dc2b8e241dd9ca84928e838297cae38e4
jeanbenoit-richez <jeanbenoit@realt.co>
2023-06-15 10:49:23 +0200

    fix: EthereumLogo

--- packages/realt-commons/src/assets/chains/EthereumLogo.tsx
+++ packages/realt-commons/src/assets/chains/EthereumLogo.tsx
@@ -8,9 +8,9 @@ export const EthereumLogo = ({ width }: SVG) => {
     // #141414
     // #393939 
     return(
-        <svg width={width ?? undefined} clip-rule="evenodd" fill-rule="evenodd" image-rendering="optimizeQuality" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" version="1.1" viewBox="0 0 784.37 1277.4"
+        <svg width={width ?? undefined} clipRule="evenodd" fillRule="evenodd" imageRendering="optimizeQuality" shapeRendering="geometricPrecision" textRendering="geometricPrecision" version="1.1" viewBox="0 0 784.37 1277.4"
             xmlns="http://www.w3.org/2000/svg">
-            <g fill-rule="nonzero">
+            <g fillRule="nonzero">
                 <polygon points="392.07 0 383.5 29.11 383.5 873.74 392.07 882.29 784.13 650.54" fill="#d9930d"/>
                 <polygon points="392.07 0 -0 650.54 392.07 882.29 392.07 472.33" fill="#f2ac26"/>
                 <polygon points="392.07 956.52 387.24 962.41 387.24 1263.3 392.07 1277.4 784.37 724.89" fill="#d9930d"/>
