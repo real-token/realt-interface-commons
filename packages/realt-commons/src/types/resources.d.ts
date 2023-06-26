f71de2624c08ef70859925a20a396275406447d7
jeanbenoit-richez <jeanbenoit@realt.co>
2023-06-26 10:55:17 +0200

    feat: add resource file type

---
+++ packages/realt-commons/src/types/resources.d.ts
@@ -0,0 +1,40 @@
+interface Resources {
+  "common": {
+    "footer": {
+      "copyright": "© {{year}} RealT. Tous droits réservés."
+    },
+    "select": {
+      "noOption": "Aucune option"
+    },
+    "settings": {
+      "title": "Langue",
+      "french": "Français",
+      "english": "Anglais",
+      "spanish": "Espagnol",
+      "light": "Clair",
+      "dark": "Sombre"
+    },
+    "wallet": {
+      "title": "Connecter mon portefeuille",
+      "control": "Connecter mon portefeuille",
+      "copy": "Copie d'adresse",
+      "viewOn": "Voir sur l'explorateur",
+      "disconnect": "Déconnexion",
+      "network": "Réseau",
+      "DisabledGnosisSafe": "La connexion directe au Safe n'est pas possible, pour se connecter à un GnosisSafe, veuillez vous rendre sur safe.global et vous connecter à votre Safe. Dans la section \"Apps\", ajoutez une \"custom app\" en utilisant l'url du site yam (https://yam.realtoken.network). Vous pourrez alors avoir le YAM directement dans l'interface de votre safe."
+    },
+    "header": {
+      "notAllowedNetwork": "Le réseau actuel n'est pas pris en charge par YAM, ",
+      "switchNetwork": "se connecter à Ethereum"
+    },
+    "general": {
+      "loading": "Chargement...",
+      "comingSoon": "Bientôt disponible...",
+      "noConnectedWallet": "S'il-vous-plaît connecter un portefeuille pour continuer (Aide : en bas à droite)",
+      "nextButton": "Suivant",
+      "noAdminError": "Vous n'êtes pas un admininistrateur"
+    }
+  }
+}
+
+export default Resources;
