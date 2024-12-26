7d60abe1d4120fdfa1efcd7ab0020b8b8d4568a2
Jean-Benoît <jeanbenoit@realt.co>
2024-12-26 14:09:17 +0100

    fix: add disabledWrongNetworkBanner on Layout

--- packages/realt-commons/src/components/layout/Layout.tsx
+++ packages/realt-commons/src/components/layout/Layout.tsx
@@ -1,48 +1,61 @@
-import { ReactNode, useEffect } from 'react';
-import { Footer, FooterParam, Header, Website, Websites } from '..';
-import { Chain, ChainSelectConfig } from '../../types';
-import classes from './Layout.module.css';
+import { ReactNode, useEffect } from "react";
+import { Footer, FooterParam, Header, Website, Websites } from "..";
+import { Chain, ChainSelectConfig } from "../../types";
+import classes from "./Layout.module.css";
 
-type LayoutProps<T> = { 
-  children: ReactNode,
+type LayoutProps<T> = {
+  children: ReactNode;
   headerNav?: React.ReactElement;
   disableHeaderMultisite?: boolean;
   head?: React.ReactElement;
   currentWebsite?: Websites;
-  chains?: ChainSelectConfig<T>|undefined;
+  chains?: ChainSelectConfig<T> | undefined;
   newWebsite?: Website;
   footerParam?: FooterParam;
   footerCustomLinks?: JSX.Element;
   headerBanner?: React.ReactElement;
   headerButtons?: React.ReactElement;
+  disabledWrongNetworkBanner?: boolean;
 };
 
-export function Layout<T extends Partial<Chain>>({ 
-  children, currentWebsite, chains, newWebsite, headerNav, head, disableHeaderMultisite, footerParam, footerCustomLinks, headerBanner, headerButtons 
-}: LayoutProps<T>){
-
+export function Layout<T extends Partial<Chain>>({
+  children,
+  currentWebsite,
+  chains,
+  newWebsite,
+  headerNav,
+  head,
+  disableHeaderMultisite,
+  footerParam,
+  footerCustomLinks,
+  headerBanner,
+  headerButtons,
+  disabledWrongNetworkBanner = false,
+}: LayoutProps<T>) {
   // TODO: move this to new logic when refactor
   useEffect(() => {
-    if(!chains) return
-    if(chains.allowedChains.length !== Object.keys(chains.chainsConfig).length) throw new Error('Allowed chains and chains config must have the same length')
-  },[chains])
+    if (!chains) return;
+    if (chains.allowedChains.length !== Object.keys(chains.chainsConfig).length)
+      throw new Error(
+        "Allowed chains and chains config must have the same length"
+      );
+  }, [chains]);
 
   return (
     <div className={classes.container}>
       {head ?? undefined}
       <Header
-        currentWebsite={currentWebsite} 
-        chains={chains} 
-        newWebsite={newWebsite} 
+        currentWebsite={currentWebsite}
+        chains={chains}
+        newWebsite={newWebsite}
         headerNav={headerNav}
         disableHeaderMultisite={disableHeaderMultisite}
         banner={headerBanner}
         headerButtons={headerButtons}
+        disabledWrongNetworkBanner={disabledWrongNetworkBanner}
       />
-      <div className={classes.main}>
-        {children}
-      </div>
-      <Footer param={footerParam} customLinks={footerCustomLinks}/>
+      <div className={classes.main}>{children}</div>
+      <Footer param={footerParam} customLinks={footerCustomLinks} />
     </div>
   );
-};
+}
