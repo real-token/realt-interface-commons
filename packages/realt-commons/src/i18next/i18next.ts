4055370b14b3a44ef0ca77a6551e9c64e2de5005
Jean-Benoît <jeanbenoit@realt.co>
2024-12-26 13:59:37 +0100

    feat: add browser language detection

--- packages/realt-commons/src/i18next/i18next.ts
+++ packages/realt-commons/src/i18next/i18next.ts
@@ -1,37 +1,43 @@
-import { initReactI18next } from 'react-i18next';
-import i18next, { Resource, i18n } from 'i18next';
-import { resources as localResources } from './locales';
-import { mergeResources } from 'i18next-resources-for-ts'
+import { initReactI18next } from "react-i18next";
+import i18next, { Resource, i18n } from "i18next";
+import { resources as localResources } from "./locales";
+import { mergeResources } from "i18next-resources-for-ts";
+import LngDetector from "i18next-browser-languagedetector";
 
-export const DEFAULT_NS = 'common';
-export const FALLBACK_LNG = 'en';
+export const DEFAULT_NS = "common";
+export const FALLBACK_LNG = "en";
 
 const initLanguage = (resources?: Resource): i18n => {
   const mergedRessources = mergeResources(localResources);
   i18next
-  .use(initReactI18next)
-  .init({
-    resources: mergedRessources,
-    defaultNS: DEFAULT_NS,
-    fallbackLng: FALLBACK_LNG,
-    debug: process.env.NODE_ENV === 'development',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+    .use(LngDetector)
+    .use(initReactI18next)
+    .init({
+      supportedLngs: ["en", "es", "fr"],
+      detection: {
+        order: ["navigator", "cookie", "localStorage"],
+      },
+      resources: mergedRessources,
+      defaultNS: DEFAULT_NS,
+      fallbackLng: FALLBACK_LNG,
+      debug: process.env.NODE_ENV === "development",
+      interpolation: {
+        escapeValue: false,
+      },
+    });
 
-  if(resources){
-    // language 
+  if (resources) {
+    // language
     for (const [lng, lngResources] of Object.entries(resources)) {
       // namespace
       for (const [namespace, nsResources] of Object.entries(lngResources)) {
         // console.log(`${namespace}: ${resources}`);
-        i18next.addResourceBundle(lng,namespace,nsResources);
+        i18next.addResourceBundle(lng, namespace, nsResources);
       }
     }
   }
 
   return i18next;
-}
+};
 
 export { initLanguage };
